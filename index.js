cc35149c0d9a434da7fc6806b72fd7d90610b605
colllijo <liamraoulpapi@gmail.com>
2023-03-08 13:52:38 +0000

    made the app work

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 
-const todos = [
-  { id: 1, text: "Learn about JS und Node" },
-  { id: 2, text: "Create a Dockerfile" },
-  { id: 3, text: "Build an image" },
-];
+app.use(express.json());
+
+const todos = [];
 
 app.get("/", (req, res) => {
   res.send("ToDo API with node");
@@ -17,6 +15,27 @@ app.get("/api/todos", (req, res) => {
 
 app.get("/api/todos/:id", (req, res) => {
   res.send(todos.find((todo) => todo.id == req.params.id));
+});
+
+app.post("/api/todos", (req, res) => {
+  let newTodo = {
+    id: todos.length ? todos[todos.length - 1].id + 1 : 1,
+    text: req.body.text
+  };
+  
+  todos.push(newTodo);
+
+  res.send(newTodo);
+});
+
+app.delete("/api/todos/:id", (req, res) => {
+  if(todos.find((todo) => todo.id == req.params.id)) {  
+      todos.splice(todos.indexOf(todos.find((todo) => todo.id == req.params.id)), 1)
+    
+      res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 const port = process.env.PORT || 3000;
